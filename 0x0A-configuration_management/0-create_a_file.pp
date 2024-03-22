225db01f79943e00572d42eebc56a9dbc806dcae
hellena254 <hellenah243@gmail.com>
2024-03-22 00:40:51 -0700

    puppet - configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 # create a file in /tmp
 
-file { '/tmp/school':
+file {'/tmp/school':
   ensure  => file,
   mode    => '0744',
   owner   => 'www-data',
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
-#!/usr/bin/pup
-
 # create a manifest that kills a process named killmenow
 # execute the pkill command to kill the process named 'killmenow'
-exec { 'killmenow':
+exec {'pkill':
   command  => 'pkill killmenow',
   provider => 'shell',
 }
