eca74d04977232d19c9270655cbcde843b643a0e
hellena254 <hellenah243@gmail.com>
2024-05-07 00:45:33 -0700

    return the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Contains the number_of_subscribers function
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    url = f"http://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "MyBot/0.1"}
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    return data["data"].get("subscribers", 0)
