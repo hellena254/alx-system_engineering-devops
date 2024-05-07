7fd85c2e2976d4156929ba16327d145af01d2c01
hellena254 <hellenah243@gmail.com>
2024-05-07 02:12:57 -0700

    debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# puppet file to fix potential issues in the WordPress
+# caused by incorrect extensions
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
